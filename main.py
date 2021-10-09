44c17d8c4525557c88fb591d5f4ad544b2ac271c
gabrielmotroni-prog <60718584+gabrielmotroni-prog@users.noreply.github.com>
2021-10-09 20:04:31 -0300

    ok

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-#arquivo main da raiz do programa para iniciar nosso programa
+# inicia a aplicacao usando __init__.py para construi o app ( pois __init__.py tem a funcao create_app )
+#esse arquivo fica na raiz do projeto - fora da pasta app
 from app import create_app
 import os
 
-#importanto a criacao do meu app da funcao create_app
+#create_app returna um app ja construido
 app = create_app()
 
-# nada abaixdo dessa funcao sera importado
+
 if __name__ == "__main__": 
+    #verificao que n deixa outros arquivos importarro que vem abaixo
     #local
     app.run(debug=True)
     #heroku
